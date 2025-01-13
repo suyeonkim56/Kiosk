32e63af54402da374176593c0dbf75fa7ddcefbd
suyeonkim <suyeonk56@gmail.com>
2025-01-13 14:26:08 +0900

    LV3 일부 코딩

--- src/main/java/com/example/kiosk/LV3/main.java
+++ src/main/java/com/example/kiosk/LV3/main.java
@@ -1,4 +1,19 @@
 package com.example.kiosk.LV3;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
+    public static void main(String[] args) {
+        MenuItem menu1 = new MenuItem(6.9,"ShackBurger","토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem menu2 = new MenuItem(8.9,"SmokeShack","베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem menu3= new MenuItem(6.9,"Cheeseburger","포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem menu4 = new MenuItem(5.4,"Hamburger","비프패티를 기반으로 야채가 들어간 기본버거");
+
+        List<MenuItem> menuItems = List.of(new MenuItem[]{menu1, menu2, menu3, menu4});
+
+        Kiosk Kiosk = new Kiosk(menuItems);
+        Kiosk.start();
+    }
 }
